f58993b1b8e94d0f6a8072e2cc7d7d948213a9b2
PrometheusKat <68077132+PrometheusKat@users.noreply.github.com>
2020-07-31 15:00:29 -0500

    removed get routes

--- hunt/config/routes.rb
+++ hunt/config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-  get 'animals/index'
-  get 'animals/show'
-  get 'animals/new'
-  get 'animals/edit'
-
   resources :animals
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #get 'animals/index'
-
   root 'animals#index'
 end
