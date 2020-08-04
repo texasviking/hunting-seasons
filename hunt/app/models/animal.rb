2bb7e93e3776f458e176eaeea5562bff631afbd0
Ramya Namuduri <rnamuduri@ML1504.local>
2020-08-04 14:10:12 -0500

    New Animal Form

--- hunt/app/models/animal.rb
+++ hunt/app/models/animal.rb
@@ -1,2 +1,14 @@
 class Animal < ApplicationRecord
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :state, presence: true
+  $CATEGORIES = ['big game','small game','migratory birds', 'fur', 'saltwater fishing', 'freshwater fishing']
+
+
+  $STATES = []
+  File.readlines("states").each do |state|
+    $STATES.append(state)
+  end
+
+
 end
