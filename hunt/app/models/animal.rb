88b72164f037cdf5da1d4bb226f85893573b81dd
PrometheusKat <68077132+PrometheusKat@users.noreply.github.com>
2020-08-04 15:34:19 -0500

    variables not global

--- hunt/app/models/animal.rb
+++ hunt/app/models/animal.rb
@@ -2,12 +2,10 @@ class Animal < ApplicationRecord
   validates :name, presence: true
   validates :category, presence: true
   validates :state, presence: true
-  $CATEGORIES = ['Big Game','Small Game','Migratory Birds', 'Fur', 'Saltwater Fishing', 'Freshwater Fishing']
-
-
-  $STATES = []
+  CATEGORIES = ['Big Game','Small Game','Migratory Birds', 'Fur', 'Saltwater Fishing', 'Freshwater Fishing']
+  STATES = []
   File.readlines("states").each do |state|
-    $STATES.append(state)
+    STATES.append(state)
   end
 
 
