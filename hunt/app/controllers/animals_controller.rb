82d4353c2c01f0510f009d2b1e57a14a5dc55809
Ramya Namuduri <rnamuduri@ML1504.local>
2020-08-05 13:48:09 -0500

    edit

--- hunt/app/controllers/animals_controller.rb
+++ hunt/app/controllers/animals_controller.rb
@@ -22,10 +22,26 @@ class AnimalsController < ApplicationController
   end
 
   def edit
+    @animal = Animal.find(params[:id])
+
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    @animal.update(animal_params)
+
+    if @animal.save
+      redirect_to animal_path(@animal)
+    else
+      render action: 'new'
+    end
+
+
 
   end
 
   private
+
 
   def animal_params
     params.require(:animal).permit(:name, :state, :category)
