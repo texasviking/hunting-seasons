3448a147bd0cc91064e2632174d9156f3b045f53
Zirou Zhang <zzhang@ML1307.local>
2020-08-05 13:09:45 -0500

    Added the edit feature

--- hunt/app/models/animal.rb
+++ hunt/app/models/animal.rb
@@ -3,10 +3,9 @@ class Animal < ApplicationRecord
   validates :category, presence: true
   validates :state, presence: true
   CATEGORIES = ['Big Game','Small Game','Migratory Birds', 'Fur', 'Saltwater Fishing', 'Freshwater Fishing']
-  STATES = []
-  File.readlines("states").each do |state|
-    STATES.append(state)
-  end
+  file = File.open("states")
+  STATES = file.readlines.map(&:chomp)
+  file.close
 
 
 end
